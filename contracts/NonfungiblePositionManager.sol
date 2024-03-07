fcfae81683b5451f6e2ea71e200597d907866bdb
Dexaran <dexaran820@gmail.com>
2024-03-08 01:16:10 +0400

    Update NonfungiblePositionManager.sol

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -2210,62 +2210,6 @@ contract DexaransNonfungiblePositionManager is
         emit IncreaseLiquidity(tokenId, liquidity, amount0, amount1);
     }
 
-    /// @dev A function that is supposed to be called to deposit liquidity in a ERC-223 token.
-    function mint223(MintParams calldata params)
-        external
-        payable
-        checkDeadline(params.deadline)
-        returns (
-            uint256 tokenId,
-            uint128 liquidity,
-            uint256 amount0,
-            uint256 amount1
-        )
-    {
-        IUniswapV3Pool pool;
-        (liquidity, amount0, amount1, pool) = addLiquidity(
-            AddLiquidityParams({
-                token0: params.token0,
-                token1: params.token1,
-                fee: params.fee,
-                recipient: address(this),
-                tickLower: params.tickLower,
-                tickUpper: params.tickUpper,
-                amount0Desired: params.amount0Desired,
-                amount1Desired: params.amount1Desired,
-                amount0Min: params.amount0Min,
-                amount1Min: params.amount1Min
-            })
-        );
-
-        _mint(params.recipient, (tokenId = _nextId++));
-
-        bytes32 positionKey = PositionKey.compute(address(this), params.tickLower, params.tickUpper);
-        (, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, , ) = pool.positions(positionKey);
-
-        // idempotent set
-        uint80 poolId =
-            cachePoolKey(
-                address(pool),
-                PoolAddress.PoolKey({token0: params.token0, token1: params.token1, fee: params.fee})
-            );
-
-        _positions[tokenId] = Position({
-            nonce: 0,
-            operator: address(0),
-            poolId: poolId,
-            tickLower: params.tickLower,
-            tickUpper: params.tickUpper,
-            liquidity: liquidity,
-            feeGrowthInside0LastX128: feeGrowthInside0LastX128,
-            feeGrowthInside1LastX128: feeGrowthInside1LastX128,
-            tokensOwed0: 0,
-            tokensOwed1: 0
-        });
-
-        emit IncreaseLiquidity(tokenId, liquidity, amount0, amount1);
-    }
-
     modifier isAuthorizedForToken(uint256 tokenId) {
         require(_isApprovedOrOwner(msg.sender, tokenId), 'Not approved');
         _;
